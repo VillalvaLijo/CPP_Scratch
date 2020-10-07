661741d99a96aa1da7a6c1ab53b38bd1a30b30f7
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-10-07 16:57:03 -0500

    Practice with integer literals

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@ int main() {
 
     int numA, numB, sum;
 
+    int chest = 42, waist = 0x42, inseam = 042;
+
     cout << "Hello, World!" << endl;
 
     cout<< " This Program takes the input of two numbers and performs addition." << endl;
 
-    cout<<"Execute Addition " << numA << " + " << numB << " + " << sum << endl;
+    cout<< "Chest: "<< chest<<"\n";
+    cout<<"Waist: "<< waist<<"\n";
+    cout<<"Inseam: "<<inseam<<"\n";
+
+    //cout<<"Execute Addition " << numA << " + " << numB << " + " << sum << endl;
 
 
     return 0;
