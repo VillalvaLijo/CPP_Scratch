3c699000b10fc42a39fe654ab7cbd683e6de23d5
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-10-07 16:17:50 -0500

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Scratch_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scratch_CPP.dir/main.cpp.o"
+  "Scratch_CPP"
+  "Scratch_CPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scratch_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+
+int addition(int numA, int numB);
+
+int main() {
+
+    using namespace std;
+
+    int numA, numB, sum;
+
+    cout << "Hello, World!" << endl;
+
+    cout<< " This Program takes the input of two numbers and performs addition." << endl;
+
+    cout<<"Execute Addition " << numA << " + " << numB << " + " << sum << endl;
+
+
+    return 0;
+}
+
+int addition(){
+
+}
