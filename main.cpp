76a11fecbd4e07f27ce6962c97a31029205ef888
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-10-07 17:36:32 -0500

    More Fun with ASCII

--- main.cpp
+++ main.cpp
@@ -26,6 +26,13 @@ int main() {
     //cout<<"Execute Addition " << numA << " + " << numB << " + " << sum << endl;
 
     cout<<"Charecter ch is "<<ch<<" to us but in ASCII it is: "<<i<<endl;
+
+    cout<<"Watch me add one to Char ch and change it's Alphabetic Value (As well as it's binary value"<<endl;
+    ch += 1;
+    cout<<"Code: ch+1"<<endl;
+    i = ch;
+
+    cout<<"New value for ch: "<<ch<<" New ASCII value: "<<i<<endl;
     return 0;
 }
 
